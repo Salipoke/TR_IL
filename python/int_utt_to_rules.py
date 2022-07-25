748209bfbca535435bb78f185f04863177a61a61
Salipoke <101245660+Salipoke@users.noreply.github.com>
2022-07-25 12:36:20 +0200

    update
    frfr

--- python/int_utt_to_rules.py
+++ python/int_utt_to_rules.py
@@ -1,25 +1,31 @@
-def rule(intents,utters):
+def rule(intent):
     exception = []
     final_str = 'version: "3.1"\n\nrules:\n\n'
-    intents = intents.split('-')
-    utters = utters.split('-')
+    intent = intent.split('-')
+    intents = []
+    utters = []
+    for k in intent:
+        if '=' not in k:
+            intents.append(k)
+            k = 'utter_'+k
+            utters.append(k) 
+        else:
+            print (k)
     for i in intents:
         intents[intents.index(i)] = i.replace(' ','')
     if intents[0] == '':
         intents.pop(0)
     for i in utters:
         utters[utters.index(i)] = i.replace(' ','')
-    if utters[0] == '':
-        utters.pop(0)
-    print(intents)
+    utters.pop(0)
+    print(len(intents))
+    print(len(utters))
     for i in intents:
-        if '=' not in i:
-            final_str = str(final_str+'- rule: Say '+utters[intents.index(i)]+' anytime the user says '+i+'\n')
-            final_str = str(final_str+'  steps:\n')
-            final_str = str(final_str+'  -  intent: '+i+'\n')
-            final_str = str(final_str+'  -  action: '+utters[intents.index(i)]+'\n\n')
-        else:
-            exception.append(i)
+        print(i)
+        final_str = str(final_str+'- rule: Say '+utters[intents.index(i)]+' anytime the user says '+i+'\n')
+        final_str = str(final_str+'  steps:\n')
+        final_str = str(final_str+'  -  intent: '+i+'\n')
+        final_str = str(final_str+'  -  action: '+utters[intents.index(i)]+'\n\n')
     print (final_str)
     print(exception)
     with open('rules.txt',mode = 'w',encoding='utf8') as file:
